2cb0524ce7faca41a92967337c95fd2201f7bfea
Mars Gabdullin <SuperEuro2000@Outlook.com>
2021-09-09 02:24:02 +0500

    second day

--- main/views.py
+++ main/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import WishList
 
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {"title": "Wishlist | about project"})
+
+
+def list_page(request, pk):
+    """
+    FBV - views основаны на функциях
+    CBV - views основаны на классах
+    """
+    wishlist = get_object_or_404(WishList, pk=pk)
+    return render(
+        request,
+        'wish_list.html',
+        {
+            'wishlist': wishlist,
+            'is_owner_list': wishlist.owner == request.user
+        }
+    )
--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,7 +1,7 @@
 """wishlist URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from main.views import index
+from main.views import index, about, list_page
 
 
 urlpatterns = [
+    # path('URL address', name_function)
     path('admin/', admin.site.urls),
-    path('', index, name='index_page')
+    path('', index, name="index_page"),
+    path('about/', about, name='about_page'),
+    path('wisher/<int:pk>/', list_page, name='wish_list_page'),
 ]
